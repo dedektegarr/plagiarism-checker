c04fad499e6c455264dc522a6c1344ca700a55e2
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2025-03-20 12:16:07 +0700

    feat: change sidebar app name

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,3 +1,4 @@
+import { appName } from '@/app';
 import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
@@ -7,7 +8,7 @@ export default function AppLogo() {
                 <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+                <span className="mb-0.5 truncate leading-none font-semibold">{appName}</span>
             </div>
         </>
     );
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
-})->name('dashboard');
+})->name('home');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', function () {
